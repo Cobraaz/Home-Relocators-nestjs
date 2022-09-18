79bcc41c434d1a064c211b6c2cc2a43ea99d0abf
Cobraaz <anujbansal077@gmail.com>
2022-09-18 11:16:59 +0530

    added redis credentials

--- src/config/cache/cache.module.ts
+++ src/config/cache/cache.module.ts
@@ -1,14 +1,19 @@
 import { CacheModule as ICacheModule, Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import redisStore from 'cache-manager-redis-store';
 import { CacheService } from './cache.service';
 
 @Global()
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ICacheModule.register({
       store: redisStore,
-      host: 'localhost',
-      port: 6379,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      username: process.env.REDIS_USERNAME, // new property
+      password: process.env.REDIS_PASSWORD, // new property
+      no_ready_check: true, // new property
       isGlobal: true,
     }),
   ],
